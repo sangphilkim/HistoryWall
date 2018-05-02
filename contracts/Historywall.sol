f907705a3369974c0d0fe65b3e5ea9065355bf7d
SangphilKim <sangphil.kim@hotmail.com>
2018-05-03 00:23:52 +0900

    delete @license when truffle compile

--- contracts/Historywall.sol
+++ contracts/Historywall.sol
@@ -5,7 +5,7 @@ import "./Stoppable.sol";
 /**
  *Base for contracts that ability to record on the History Wall.
  *
- *@license GNU General Public License 3.0, http:www.gnu.org/licenses/gpl-3.0.en.html
+ *license GNU General Public License 3.0, http:www.gnu.org/licenses/gpl-3.0.en.html
  *@author Sangphil Kim, https://www.linkedin.com/in/sangphilkim/
  */
 
