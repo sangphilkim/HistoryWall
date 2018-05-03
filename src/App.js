8cabfd5bbb349b036752ab016ee15ef5482a6055
SangphilKim <sangphil.kim@hotmail.com>
2018-05-03 20:39:57 +0900

    modify index.html

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ class App extends Component {
       })
     })
   }
-
+/*
   render() {
     return (
       <div className="App">
@@ -85,6 +85,7 @@ class App extends Component {
       </div>
     );
   }
+*/
 }
 
 export default App
